ff82788348ff3b5b3fca191d137d6670fa3f6e9e
Superstar64 <Superstar64@gmail.com>
2014-05-09 14:45:26 -0400

    minor bug fix for cache range(first front wasn't working)

--- tpool/range.d
+++ tpool/range.d
@@ -1,6 +1,10 @@
 import std.range;
 
 struct Cache(R,F) if(isInputRange!R){
+	this(R range_){
+		range=range_;
+		front=range.front;
+	}
 	R range;
 	F front;
 	void popFront(){
@@ -16,8 +20,12 @@ auto cache(R)(R range){
 unittest{
 	ubyte[] ar=[1,2,3];
 	auto range=cache(ar);
+	assert(range.front==1);
 	range.front=2;
 	assert(ar==[1,2,3]);
+	range.popFront();
+	assert(range.front==2);
+	
 	ar.front=2;
 	assert(ar==[2,2,3]);
 }
