dd9161ba43de271502ed5f7b4fd8105b8c187a2e
Superstar64 <Superstar64@gmail.com>
2014-05-25 17:12:18 -0400

    added onPop Range

--- tpool/range.d
+++ tpool/range.d
@@ -1,5 +1,5 @@
 import std.range;
-
+import tpool.stream.common:autoSave;
 struct Cache(R,F) if(isInputRange!R){
 	this(R range_){
 		range=range_;
@@ -7,11 +7,12 @@ struct Cache(R,F) if(isInputRange!R){
 	}
 	R range;
 	F front;
-	void popFront(){
+	@property void popFront(){
 		range.popFront();
 		front=range.front;
 	}
 	alias range this;
+	mixin autoSave!(range);
 }
 auto cache(R)(R range){
 	return Cache!(R,ElementType!(R))(range);
@@ -28,4 +29,24 @@ unittest{
 	
 	ar.front=2;
 	assert(ar==[2,2,3]);
+}
+struct OnPop(Range,alias fun) if(isInputRange!Range){
+	Range r;
+	mixin autoSave!r;
+	@property auto popFront(){
+		fun();
+		r.popFront;
+	}
+}
+template onPop(alias fun){
+	auto onPop(Range)(Range r){
+		return OnPop!(Range,fun)(r);
+	}
+}
+unittest {
+	ubyte[]a =[0,1];
+	bool test=false;
+	auto range=onPop!(()=>test=true)(a);
+	range.popFront;
+	assert(test);
 }
