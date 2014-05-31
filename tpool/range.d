1620420e96889bca9ea5f8486952d4cfe0e91772
Superstar64 <Superstar64@gmail.com>
2014-05-30 20:25:55 -0400

    removed alias this from tpool/range because of safety concerns,added CRange and OEmpty

--- tpool/range.d
+++ tpool/range.d
@@ -8,11 +8,15 @@ struct Cache(R,F) if(isInputRange!R){
 	}
 	R range;
 	F front;
-	@property void popFront(){
-		range.popFront();
-		front=range.front;
+	@property{
+		void popFront(){
+			range.popFront();
+			front=range.front;
+		}
+		auto empty(){
+			return range.empty;
+		}
 	}
-	alias range this;
 	mixin autoSave!(range);
 }
 auto cache(R)(R range){
@@ -33,7 +37,6 @@ unittest{
 }
 struct OnPop(Range,alias fun) if(isInputRange!Range){
 	Range r;
-	alias r this;
 	mixin autoSave!r;
 	@property auto popFront(){
 		static if(is(typeof((inout int=0){
@@ -45,6 +48,12 @@ struct OnPop(Range,alias fun) if(isInputRange!Range){
 		}
 		r.popFront;
 	}
+	@property auto front(){
+		return r.front;
+	}
+	@property auto empty(){
+		return r.empty;
+	}
 }
 template onPop(alias fun){
 	auto onPop(Range)(Range r){
@@ -55,6 +64,75 @@ unittest {
 	ubyte[]a =[0,1];
 	bool test=false;
 	auto range=onPop!(a=>test=true)(a);
+	static assert(isForwardRange!(typeof(range)));
+	auto r2=range.save;
 	range.popFront;
 	assert(test);
+}
+
+struct CRange(alias gfront,alias gempty){//create range
+	@property 
+	{
+		auto front(){
+			return gfront();
+		}
+		void popFront(){
+		}
+		bool empty(){
+			return gempty();
+		}
+	}
+}
+auto cRange(alias gfront,alias gempty)(){
+	return CRange!(gfront,gempty)();
+}
+unittest{
+	uint cur;
+	auto range=cRange!(()=>{cur++;return cur;}(),()=>cur==5);
+	static assert(isInputRange!(typeof(range)));
+	assert(range.front==1);
+	assert(!range.empty);
+	assert(range.front==2);
+	assert(!range.empty);
+	assert(range.front==3);
+	assert(!range.empty);
+	assert(range.front==4);
+	assert(!range.empty);
+	assert(range.front==5);
+	assert(range.empty);
+}
+
+struct OEmpty(Range,alias gempty) if(isInputRange!Range){//override empty
+	Range r;
+	mixin autoSave!(r);
+	@property bool empty(){
+		static if(is(typeof((inout int=0){
+			return gempty(r);
+		}))){
+			return gempty(r);
+		}else{
+			return gempty();
+		}
+	}
+	@property{
+		auto popFront(){
+			return r.popFront;
+		}
+		
+		auto front(){
+			return r.front;
+		}
+	}
+}
+template oEmpty(alias fun){
+	auto oEmpty(Range)(Range r){
+		return OEmpty!(Range,fun)(r);
+	}
+}
+unittest {
+	import std.stdio;
+	ubyte[] mem=[0,1,2,3];
+	auto range=oEmpty!(a=>a.front==2)(mem);
+	static assert(isForwardRange!(typeof(range)));
+	assert(range.array==[0,1]);
 }
