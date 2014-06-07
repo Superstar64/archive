b3bdd9e0422e07dbf428119107ee7b37a7472cb1
Superstar64 <Superstar64@gmail.com>
2014-06-07 10:02:24 -0400

    added comment and gzipIRangeRStream to gzip.d

--- tpool/format/gzip.d
+++ tpool/format/gzip.d
@@ -5,7 +5,11 @@ import tpool.stream.wstream;
 //http://www.gzip.org/zlib/rfc-gzip.html
 
 import etc.c.zlib;
-auto gzipRStream(RStream)(RStream stream,void[] buffer){
+auto gzipIRangeRStream(Range)(Range range){//constructs gzip stream of inputrange of void[]
+	return zlibIRangeRStream!(a=>inflateInit2(a,15|16))(range);
+}
+
+auto gzipRStream(RStream)(RStream stream,void[] buffer){//constructs gzip rstream around sub rstream
 	return zlibRStream!(a=>inflateInit2(a,15|16))(stream,buffer);
 }
 unittest{
@@ -13,7 +17,7 @@ unittest{
 	auto a=gzipRStream(MemRStream(),buf);
 }
 
-auto gzipWStream(RStream)(RStream stream,void[] buf,int compress=-1){
+auto gzipWStream(RStream)(RStream stream,void[] buf,int compress=-1){//constructs gzip wstream and sub wstream
 	return zlibWStream!((a,b)=>deflateInit2(a,b,Z_DEFLATED,15|16,8,Z_DEFAULT_STRATEGY))(stream,buf,compress);
 }
 
